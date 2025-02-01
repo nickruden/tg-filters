c4c6b905e926d7b1d669988e353227d960f25a24
nickruden <1nikitos.ru2018@gmail.com>
2025-02-01 20:11:48 +0300

    Логика блока категории

--- assets/scripts/filter-dataPicker.js
+++ assets/scripts/filter-dataPicker.js
@@ -22,6 +22,7 @@ document.addEventListener('DOMContentLoaded', function () {
             // Инициализация Flatpickr
             const flatpickrInstance = initFlatpickr(inputId, {
                 dateFormat: 'd.m.Y',
+                locale: 'ru',
                 minDate: isFromInput ? 'today' : null, // Для поля "от" запрещаем прошедшие даты
                 disableMobile: 'true',
                 onOpen: function (selectedDates, dateStr, instance) {
